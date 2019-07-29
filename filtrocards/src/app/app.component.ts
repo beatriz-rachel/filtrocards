5c2784608605dc4152dad346649cd9dbb4ddea6f
beatriz-rachel <beatrizrachel.vbp@gmail.com>
2019-07-29 17:16:25 -0300

    instalação do bootstrap e criação form

--- filtrocards/src/app/app.component.ts
+++ filtrocards/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'filtrocards';
+
+  public frmBusca: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.frmBusca = this.fb.group({
+      filtro: this.fb.control(''),
+    });
+  }
+
+  public teste(): void {
+    console.log('clicou');
+  }
 }
